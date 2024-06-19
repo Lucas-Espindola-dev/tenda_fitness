c50a567d652ac887063c7d25b5d9d89f842d8a6c
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-06-19 08:14:42 -0300

    Adding fields to models

--- schedules/models.py
+++ schedules/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 
+SPORT_CHOICES = (
+    ('BEACH TENIS', 'Beach tennis'),
+    ('VOLEI', 'Volei'),
+    ('FUTVOLEI', 'Futvolei')
+)
+
 class Appointment(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    date = models.DateField()
+    date_appointment = models.DateField()
+    date_created = models.DateTimeField(auto_now_add=True)
     time = models.TimeField()
+    sport = models.CharField(max_length=255, choices=SPORT_CHOICES, blank=True, null=True)
