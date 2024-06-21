8f63d09a119ca5ec5baa6a66a53e567bf91e3284
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-06-21 11:09:04 -0300

    Creating the serializer class

--- schedules/views.py
+++ schedules/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from schedules.models import Appointment
+from schedules.serializers import AppointmentModelSerializer
+import datetime
 
-# Create your views here.
+
+class AppointmentCreateListView(generics.ListCreateAPIView):
+    serializer_class = AppointmentModelSerializer
+    queryset = Appointment.objects.all()
+
+
+class AppoitmentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = AppointmentModelSerializer
+    queryset = Appointment.objects.all()
+
+
