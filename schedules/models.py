dd38554562b889b17d020490edde822963258524
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-06-21 07:26:53 -0300

    Adding the time choices

--- schedules/models.py
+++ schedules/models.py
@@ -6,11 +6,19 @@ SPORT_CHOICES = (
     ('VOLEI', 'Volei'),
     ('FUTVOLEI', 'Futvolei')
 )
+TIME_CHOICES = (
+    ('17:00 - 18:00', '17:00 - 18:00'),
+    ('18:00 - 19:00', '18:00 - 19:00'),
+    ('19:00 - 20:00', '19:00 - 20:00'),
+    ('20:00 - 21:00', '20:00 - 21:00'),
+    ('21:00 - 22:00', '21:00 - 22:00'),
+)
+
 
 class Appointment(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    date_appointment = models.DateField()
+    day = models.DateField()
+    time = models.CharField(max_length=255, choices=TIME_CHOICES, blank=True, null=True)
     date_created = models.DateTimeField(auto_now_add=True)
-    time = models.TimeField()
     sport = models.CharField(max_length=255, choices=SPORT_CHOICES, blank=True, null=True)
