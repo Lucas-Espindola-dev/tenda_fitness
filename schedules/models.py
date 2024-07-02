535b6f7b44661f342d93a93f99d3c7baee22b312
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-07-02 16:29:22 -0300

    Changing time field to a TimeField instead of a Choice field

--- schedules/models.py
+++ schedules/models.py
@@ -8,20 +8,13 @@ SPORT_CHOICES = (
     ('VOLEI', 'Volei'),
     ('FUTVOLEI', 'Futvolei')
 )
-TIME_CHOICES = (
-    ('17:00 - 18:00', '17:00 - 18:00'),
-    ('18:00 - 19:00', '18:00 - 19:00'),
-    ('19:00 - 20:00', '19:00 - 20:00'),
-    ('20:00 - 21:00', '20:00 - 21:00'),
-    ('21:00 - 22:00', '21:00 - 22:00'),
-)
 
 
 class Appointment(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     day = models.DateField()
-    time = models.CharField(max_length=255, choices=TIME_CHOICES,)
+    time = models.TimeField()
     date_created = models.DateTimeField(auto_now_add=True)
     sport = models.CharField(max_length=255, choices=SPORT_CHOICES, blank=True, null=True)
     repeat = models.BooleanField(default=False)
