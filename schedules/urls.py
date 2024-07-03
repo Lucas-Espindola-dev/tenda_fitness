39fbea48b9169bb6ed6e4dfc6e595ea7a8e08f6c
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-07-03 09:01:05 -0300

    Adding the paths to users slots and availible slots

--- schedules/urls.py
+++ schedules/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from schedules.views import AppointmentCreateListView, AppoitmentRetrieveUpdateDestroyView
+from schedules.views import AppointmentCreateListView, AppoitmentRetrieveUpdateDestroyView, UserAppointmentsView, AvailibleSlotsView
 
 
 urlpatterns = [
     path('appointment/', AppointmentCreateListView.as_view(), name='appointment-create-list'),
     path('appointment/<int:pk>/', AppoitmentRetrieveUpdateDestroyView.as_view(), name='appointment-detail-view'),
+    path('users/<str:username>/bookings/', UserAppointmentsView.as_view(), name='user-bookings'),
+    path('availible-slots/', AvailibleSlotsView.as_view(), name='availible-slots'),
 ]
