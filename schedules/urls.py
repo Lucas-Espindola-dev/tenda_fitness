2e383f0cb7517e1cc113eb0cc6e0f818ed63c5e0
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-07-10 08:02:17 -0300

    Excluding the path to only availible slots

--- schedules/urls.py
+++ schedules/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path('api/appointment/', views.AppointmentCreateListAPIView.as_view(), name='appointment-create-list'),
     path('api/appointment/<int:pk>/', views.AppoitmentRetrieveUpdateDestroyAPIView.as_view(), name='appointment-detail-view'),
     path('api/users/<str:username>/bookings/', views.UserAppointmentsAPIView.as_view(), name='user-bookings'),
-    path('api/availible-slots/', views.AvailibleSlotsAPIView.as_view(), name='availible-slots'),
 
     # Urls para o site.
     path('appointments/', views.AppointmentListView.as_view(), name='appointments-list'),
--- schedules/views.py
+++ schedules/views.py
@@ -67,20 +67,6 @@ class AppoitmentRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIVi
     queryset = Appointment.objects.all()
 
 
-class AvailibleSlotsAPIView(generics.ListAPIView):
-    serializer_class = AvailiableSlotsSerializer
-
-    def get_queryset(self):
-        day = self.request.query_params.get('day')
-        if day:
-            try:
-                day = datetime.strptime(day, '%Y-%m-%d').date()
-                return [{'day': day}]
-            except ValueError:
-                return []
-        return []
-
-
 class UserAppointmentsAPIView(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserAppointmentsSerializer
