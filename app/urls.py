a252eb37fd01df7fd18ce47b34a70d8fea3d65b8
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-07-12 10:02:44 -0300

    Adding the paths to register a new user and the user path.

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,8 @@ from django.contrib.auth import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('schedules.urls')),
+    path('register/', ..., name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
+    path('user/<str:username>/'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
