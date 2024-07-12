53107df13bfc97d5c66a64e9e4ce43c703a5a9d3
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-07-12 09:58:04 -0300

    Adding the url to home page.

--- schedules/urls.py
+++ schedules/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('api/users/<str:username>/bookings/', views.UserAppointmentsAPIView.as_view(), name='user-bookings'),
 
     # Urls para o site.
+    path('', views.home, name='home'),
     path('appointments/', views.AppointmentListView.as_view(), name='appointments-list'),
     path('appointments/new/', views.AppointmentCreateView.as_view(), name='appointments-create'),
     path('users/<str:username>/appointments/', views.UserAppointmentsListView.as_view(), name='user-appointments-list'),
