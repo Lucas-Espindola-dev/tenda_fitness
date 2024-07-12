199da7be60fd43235f6a190cce9d15073aba7bc0
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-07-12 15:21:29 -0300

    Creating the login view

--- accounts/views.py
+++ accounts/views.py
@@ -12,3 +12,18 @@ def register_view(request):
     else:
         user_form = UserCreationForm()
     return render(request, ..., {'user_form': user_form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('user-appointments-list')
+        else:
+            login_form = AuthenticationForm()
+    else:
+        login_form = AuthenticationForm()
+    return render(request, ..., {"login_form": login_form})
