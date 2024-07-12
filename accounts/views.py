397680bd1ab8624df4c70e32bdcf1bcb55ca4e7f
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-07-12 14:58:59 -0300

    Creating the register view to new users

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+def register_view(request):
+    if request.method == 'POST':
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect("login")
+    else:
+        user_form = UserCreationForm()
+    return render(request, ..., {'user_form': user_form})
