515e08f19cb3ba260d5bf225a5e95c03e82cdcd2
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-07-15 16:46:44 -0300

    Adding the register html

--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,7 @@ def register_view(request):
             return redirect("login")
     else:
         user_form = UserCreationForm()
-    return render(request, ..., {'user_form': user_form})
+    return render(request, 'accounts/register.html', {'user_form': user_form})
 
 
 def login_view(request):
