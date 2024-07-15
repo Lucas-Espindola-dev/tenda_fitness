b3d1a86462c3c67d670661f6ce0467b0bf76a79b
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-07-15 16:42:48 -0300

    Creating the register url

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views
-from accounts.views import login_view, logout_view
+from accounts.views import login_view, logout_view, register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('schedules.urls')),
-    # path('register/', ..., name='register'),
+    path('register/', register_view, name='register'),
     path('login/', login_view, name='login'),
     # path('user/<str:username>/'),
     path('logout/', logout_view, name='logout'),
