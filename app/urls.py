d1c12a9a1ac345c677d9329f6e0bf500817b87c0
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-07-15 15:57:29 -0300

    Correcting the views

--- app/urls.py
+++ app/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views
+from accounts.views import login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('schedules.urls')),
-    path('register/', ..., name='register'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('user/<str:username>/'),
+    # path('register/', ..., name='register'),
+    path('login/', login_view, name='login'),
+    # path('user/<str:username>/'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
 ]
