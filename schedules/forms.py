da2276f2af602ba0ab131a626a785bff0931c671
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-07-17 10:24:15 -0300

    .

--- schedules/forms.py
+++ schedules/forms.py
@@ -6,7 +6,7 @@ class AppointmentForm(forms.ModelForm):
 
     class Meta:
         model = Appointment
-        fields = ['day', 'time', 'sport', 'repeat',]
+        fields = ['day', 'time', 'sport', 'repeat', ]
         widgets = {
             'day': forms.DateInput(attrs={'type': 'date'}),
             'time': forms.TimeInput(attrs={'type': 'time'}),
