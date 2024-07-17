0a47f91a68bad92662099b902d2388872b4c9c32
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-07-17 16:56:45 -0300

    Creating the time admin model

--- schedules/admin.py
+++ schedules/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import Appointment
+from .models import Appointment, Time
 
 
 class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ('id', 'user', 'day', 'time', 'sport',)
+    list_display = ('id', 'user', 'day', 'time',)
 
 
 admin.site.register(Appointment, AppointmentAdmin)
+
+
+class TimeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'hours',)
+
+
+admin.site.register(Time, TimeAdmin)
