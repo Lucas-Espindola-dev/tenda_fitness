d55caca2c593c4a13835052f624cfac4cc5e87b4
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-07-29 10:20:09 -0300

    Creating an Update path to appointments

--- schedules/urls.py
+++ schedules/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('appointments/new/', views.AppointmentCreateView.as_view(), name='appointments-create'),
     path('users/<str:username>/appointments/', views.UserAppointmentsListView.as_view(), name='user-appointments-list'),
     path('appointments/sucess/', views.sucess_message, name='sucess'),
+    path('appointments/<int:pk>/update/', views.AppointmentUpdateView.as_view(), name='appointment-update'),
     path('appointments/<int:pk>/delete/', views.AppointmentDeleteView.as_view(), name='appointment-delete'),
 ]
