5ddcf12a6b096b6ad237c5498c4f3085b5067bb3
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-07-29 09:05:44 -0300

    Creating the delete appointment path

--- schedules/urls.py
+++ schedules/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('appointments/', views.AppointmentListView.as_view(), name='appointments-list'),
     path('appointments/new/', views.AppointmentCreateView.as_view(), name='appointments-create'),
     path('users/<str:username>/appointments/', views.UserAppointmentsListView.as_view(), name='user-appointments-list'),
-    path('appointments/sucess/', views.sucess_message, name='sucess')
+    path('appointments/sucess/', views.sucess_message, name='sucess'),
+    path('appointments/<int:pk>/delete/', views.AppointmentDeleteView.as_view(), name='appointment-delete'),
 ]
